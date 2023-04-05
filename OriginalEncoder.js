7bd12545984e7b37580caade8b15aefde8fed0fc
FelipeImhoff <felipe_imhoff@hotmail.com>
2023-04-05 19:23:38 -0300

    Adjusted the offset calculation

--- OriginalEncoder.js
+++ OriginalEncoder.js
@@ -1,28 +1,27 @@
 // Constants
-const prompt = require('prompt-sync')();
+const prompt = require("prompt-sync")();
 
 // Receiving messages to encode by prompt
-const input = prompt('Enter a message to encode: ')
+const input = prompt("Enter a message to encode: ");
 
 // Function to encrypt original text
 function encrypt(str) {
-  let result = str.split('').map(char =>{
+  let result = str.split("").map((char) => {
     // If the character is space send back space to encoded string
-    if(char === ' ') {
-     return char
+    if (char === " ") {
+      return char;
     }
     // Get the ascii number of the character
-    let ascii = char.charCodeAt()
+    let ascii = char.charCodeAt();
 
     // Set the offset based on the regular expression
-    let offset = /[y-zY-Z]/.test(char) ? 24 : 2
+    let offset = /[y-zY-Z]/.test(char) ? -24 : 2;
 
     // Get the character of value from the sum of the character plus the offset
-    return String.fromCharCode(ascii + offset)
-  })
-  return result.join('')
+    return String.fromCharCode(ascii + offset);
+  });
+  return result.join("");
 }
 
-
 // call function and showing the result
-console.log(encrypt(input))
+console.log(encrypt(input));
